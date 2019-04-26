1922264315e69edd7ece0ad53c65373457a13e79
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-26 08:55:56 -0700

    Adding character data to my Character component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -7,6 +7,10 @@ function Character (props) {
         <div>
             {/* Receiving each character's Data via a prop, the Character component can then call on different data on each CharacterData element */}
             <h1>{props.characterData.name}</h1>
+            <h3>Birth year: {props.characterData.birth_year}</h3>
+            <h3>Gender: {props.characterData.gender}</h3>
+            <h3>Height: {props.characterData.height}</h3>
+            <h3>Mass: {props.characterData.mass}</h3>
         </div>
     )
 }
